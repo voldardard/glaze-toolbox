f85c228945e0596288594afaf25fc0bd080f83e9
MAthias Brugger <mbrugger@net4all.ch>
2020-05-20 12:10:24 +0200

    Register controller

--- app/Http/Controllers/auth/Register.php
+++ app/Http/Controllers/auth/Register.php
@@ -16,35 +16,17 @@ use Lang;
 
 
 
-class Login extends Controller{
+class Register extends Controller{
 
     public function __invoke(Request $request){
         $validatedData = $request->validate([
             'user' => 'required|string',
+            'name' => 'required|string',
+            'fsname' => 'required|string',
+            'email' => 'required|email',
             'password' => 'required|string',
         ]);
 
-        if(! self::test_connection($request->input('user'), $request->input('password'))){
-            return Redirect::back()->withError( Lang::get('login.l-007-usernameorpasswordmissmatch'))->withInput();
-        }
         return Redirect::to(route('action'));
-    }
-    private function test_connection($login, $password){
-        $rounds=(int)(strlen($login.env('ROUNDS').$password)/2);
-        $hashedpassword =Hash::make(env('SALT1').$password.env('SALT2'), ['rounds' => $rounds]);
-
-        if(DB::table('users')->where(['username'=>$login, 'password'=>$hashedpassword])->exists()){
-            session(DB::table('users')->select("name", "fsname", "username", "id", "email", "admin")->where('username', $login)->first());
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public function logout(Request $request){
-        $request->session()->flush();
-        $request->session()->regenerate();
-        return Redirect::to(route('login'))->withError(  Lang::get('login.l-008-disconnectSuccess') )->withInput();
-
     }
 }
