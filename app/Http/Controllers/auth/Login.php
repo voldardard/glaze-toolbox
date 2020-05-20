716677bd3789750e86103b1fae48e4d81ab1fc66
MAthias Brugger <mbrugger@net4all.ch>
2020-05-20 11:25:19 +0200

    adjust migration and login post

--- app/Http/Controllers/auth/Login.php
+++ app/Http/Controllers/auth/Login.php
@@ -13,11 +13,8 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
 
 
+
 class Login extends Controller{
-
-    private $success=false;
-    private $message;
-
 
     public function __invoke(Request $request){
         $validatedData = $request->validate([
@@ -25,50 +22,29 @@ class Login extends Controller{
             'password' => 'required|string',
         ]);
 
-        if(self::test_connection($request->input('user'), $request->input('password'))){
-            self::store_data($request->input('user'), $request->input('password'));
-        }else{
-            return Redirect::back()->withError( $this->message )->withInput();
+        if(! self::test_connection($request->input('user'), $request->input('password'))){
+            return Redirect::back()->withError( "Username or password missmatch")->withInput();
         }
         return Redirect::to(route('action'));
     }
     private function test_connection($login, $password){
-        $client = new Client(['http_errors' => false, 'headers' => ["X-AUTH-USER"=>$login, "X-AUTH-TOKEN"=>$password]]);
+        $rounds=(int)(count_chars($login.env('ROUNDS').$password)/2);
+        $hashedpassword =Hash::make(env('SALT1').$password.env('SALT2'), ['rounds' => $rounds]);
 
-        $request = $client->get(env('KIMAI_API_URL') . 'ping');
+        if(DB::table('users')->where(['username'=>$login, 'password'=>$hashedpassword])->exists()){
+            self::store_data($login);
 
-        $response = json_decode($request->getBody()->getContents());
-        $this->message=$response->message;
-
-        if($response->message=="pong") return true;
-        else return false;
-    }
-    private function store_data($login, $password){
-        //generated dynamic token
-        $token= hash("sha512", env('APP_SALT_PART1') . $login . env('APP_SALT_PART2').date("Y-m-d_H:i:s").env('APP_SALT_PART3'));
-        //cut token in 2 parts
-        $middle = 64;
-        $token1 = substr($token, 0, $middle);
-        $token2 = substr($token, $middle);
-
-        $crypted_password = Crypt::encrypt($token1. openssl_encrypt($password,env('APP_CRYPT_METHOD'), env('APP_CRYPT_KEY')). $token2);
-
-        //insert user if not exist or update password if needed
-        if(! DB::table('users')->select('id')->where('username', $login)->exists()){
-            $id = DB::table('users')->insertGetId(["username"=>$login, "password"=>$crypted_password, "created_at"=>now(), "updated_at"=>now()]);
+            return true;
         }else{
-            $id = DB::table('users')->select('id')->where('username', $login)->first()->id;
-            DB::table('users')->where('id', $id)->update(["password"=>$crypted_password, "updated_at"=>now()]);
+            return false;
         }
+    }
+    private function store_data($login){
+        $user = DB::table('users')->where('username', $login)->first();
 
-        Cookie::queue("token", $token1);
-        session(["token"=>$token2]);
-        session(["username"=>$login]);
-        session(["id"=>$id]);
-
+        session(["name"=>$user->name, "fsname"=>$user->fsname, "username"=>$user->username, "id"=>$user->id, "email"=>$user->email]);
 
     }
-
     public function logout(Request $request){
         $request->session()->flush();
         $request->session()->regenerate();
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->timestamps();
+            $table->boolean('admin')->default(false);
+
         });
     }
 
