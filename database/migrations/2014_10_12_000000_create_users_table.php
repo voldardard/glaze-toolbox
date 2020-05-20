136d487ddd9c66cb216572235a31302510f5596a
MAthias Brugger <mbrugger@net4all.ch>
2020-05-20 10:14:53 +0200

    Create db migration file to create user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('fsname');
             $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->index('username');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
