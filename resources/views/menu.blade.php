2a3e3d242414a34a71ac9e95a8df614a893434d3
MAthias Brugger <mbrugger@net4all.ch>
2020-05-20 20:58:17 +0200

    adjust menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -109,20 +109,25 @@
     #cssmenu > ul > li > a:hover:after {
         background: #5C5C5C;
     }
+    #cssmenu #langs{
+        display:none;
+    }
 </style>
 <div id='cssmenu'>
     <ul>
         <li class='active'><a href='/{{ app()->getLocale() }}/'>@lang('menu.m-001-home')</a></li>
         <li><a href='/{{ app()->getLocale() }}/categories'>@lang('menu.m-002-categories')</a></li>
         <li><a href='/{{ app()->getLocale() }}/list'>@lang('menu.m-003-list')</a></li>
-        <li class="right">
-            <a href="#">Languages</a>
-            @foreach (Config::get('app.availables_locale') as $lang => $language)
-                <span id="{{$lang}}" >{{$language}}</span>
-            @endforeach
-        </li>
+
         <li class="right"><a href='#'>{{session('username')}}</a></li>
         <li class="right"><a href='/{{ app()->getLocale() }}/logout'>@lang('menu.m-004-logout')</a></li>
-
+        <li class="right">
+            <a href="#">Languages</a>
+            <div id="langs">
+                @foreach (Config::get('app.availables_locale') as $lang => $language)
+                    <a href="href='/{{$lang}}" >{{$language}}</a>
+                @endforeach
+            </div>
+        </li>
     </ul>
 </div>
