541a150e2e71364c03b57fe2dafb7c8bd25ea629
MAthias Brugger <mbrugger@net4all.ch>
2020-05-21 21:53:26 +0200

    adjust script

--- public/assets/js/insert.js
+++ public/assets/js/insert.js
@@ -1,15 +1,17 @@
-function add_label(){
+var label_count=0;
 
-    var count = document.getElementById("labels").value;
+function add_label(){
+    label_count++;
+
 
     var container = document.getElementById("labels");
     var input = document.createElement('input');
     input.setAttribute('type', 'text');
     input.setAttribute('placeholder', 'Label');
-    input.setAttribute('name', 'label['+(Number(count)+1)+']');
+    input.setAttribute('name', 'label['+label_count+']');
 
     container.appendChild(input);
 
-    document.getElementById("labels").value=(Number(count)+1);
+    document.getElementById("labels").value=label_count;
 
 }
