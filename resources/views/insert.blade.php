9d97a489e074007e424f0ec256abfa8aefd58ed8
MAthias Brugger <mbrugger@net4all.ch>
2020-05-29 00:55:22 +0200

    add js header , accept json

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -31,6 +31,15 @@
 <body>
 <div id="alert">
 
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+        <div class="alert alert-danger alert-dismissible fade show fixed-top">
+            {{ $error }}
+            <button class="close" type="button" data-dissmiss="alert">x</button>
+        </div>
+        @endforeach
+    @endif
+
 </div>
 
 @include('menu')
