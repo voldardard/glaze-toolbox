614925100139c19195bb9bc4650c643eb742c5a6
MAthias Brugger <mbrugger@net4all.ch>
2020-08-07 15:20:12 +0200

    add autoremove for belowed categories

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -59,8 +59,11 @@
                 <div id="categories">
                     <div class="autocomplete" level="0">
                         <input type="text" id="add-categories" value="{{ old('category') }}" required name="category[0]" placeholder="@lang('insert.i-007-category')"/>
+                        <a id="categories-remove-0" class="remove-label" onclick="remove_category(0)"><i class="fa fa-minus-square" aria-hidden="true"></i></a>
                     </div>
                 </div>
+                <div><a class="insert-label" onclick="add_category()" ><i class="fa fa-plus-circle" aria-hidden="true"></i> Add subcategories</a></div>
+
                 <div id="labels">
 
                     <div><a class="insert-label" onclick="add_label()" ><i class="fa fa-plus-circle" aria-hidden="true"></i> @lang('insert.i-008-addlabel')</a></div>
