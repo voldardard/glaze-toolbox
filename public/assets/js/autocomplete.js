1b80370f02c27a7f10772eb9582b6a34132b50d0
MAthias Brugger <mbrugger@net4all.ch>
2020-08-08 13:26:36 +0200

    debug saving to db

--- public/assets/js/autocomplete.js
+++ public/assets/js/autocomplete.js
@@ -153,13 +153,14 @@ function autocomplete_raw(inp, arr, level) {
                 /*create a DIV element for each matching element:*/
                 b = document.createElement("DIV");
                 /*make the matching letters bold:*/
-                b.innerHTML = "<strong>" + arr[i]['name'].substr(0, val.length) + "</strong> / " + arr[i]['formula'];
+                b.innerHTML = "<div class='first-input'><strong>" + arr[i]['name'].substr(0, val.length) + "</strong></div>";
+                b.innerHTML += "<div class='second-input'>" + arr[i]['formula'] + "</div>";
 
                 b.innerHTML += arr[i]['name'].substr(val.length);
                 /*insert a input field that will hold the current array item's value:*/
                 b.innerHTML += "<input id='" + arr[i]['id'] + "' type='hidden' formula='" + arr[i]['formula'] + "' value='" + arr[i]['name'] + "'>";
                 /*execute a function when someone clicks on the item value (DIV element):*/
-                b.addEventListener("click", function(e) {
+                b.addEventListener("click", function (e) {
 
                     /*insert the value for the autocompleted text field:*/
                     inp.setAttribute('value', (this.getElementsByTagName("input")[0].value));
