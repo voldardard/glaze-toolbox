ec1cc9ac1bef822221505748ed4ea53b65749e6f
Brugger Mathias <brugger.mathias@hotmail.com>
2020-08-12 17:28:12 +0200

    display creator

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -49,6 +49,7 @@
         @foreach($Params->categories as $category)
             <span class="block"><a href="/category/{{$category->id}}"><i class="fa fa-chevron-right"></i> {{$category->name}}</a></span>
         @endforeach
+        <i class="fa fa-chevron-right"></i>
         <h1 class="block">{{$Params->name}}</h1><span class="block">{{$Params->version}}</span>
         <div id="author">
             <span>Created by {{$Params->creator->fsname." ".$Params->creator->name}}</span>
