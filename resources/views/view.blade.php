2f7e807e1a9e27f88417009b293f953d17b034c3
Brugger Mathias <brugger.mathias@hotmail.com>
2020-08-17 22:27:54 +0200

    display creator

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -22,7 +22,7 @@
 
 
     <!-- Scripts -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>-->
     <script src="{{ asset('js/carousel.js') }}"></script>
     <script type="text/javascript">
