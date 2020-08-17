5bbfc7dabd354ed7a2df18f6c94194897234a31a
Brugger Mathias <brugger.mathias@hotmail.com>
2020-08-17 22:19:28 +0200

    display creator

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -18,11 +18,13 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/menu.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/view.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/carousel.css') }}">
 
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/carousel.js') }}"></script>
     <script type="text/javascript">
         var locale = "{{ str_replace('_', '-', app()->getLocale()) }}";
 
@@ -62,37 +64,17 @@
     </div>
     @if(!empty($Params->pictures))
         <div id="carousel" class="container">
-            <br>
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    @for($i=0; $i< count($Params->pictures); $i++)
-                        <li data-target="#myCarousel" data-slide-to="{{$i}}" @if($i==0) class="active" @endif></li>
-                    @endfor
-                </ol>
+            <div class="slider-container">
 
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                    @foreach($Params->pictures as $key => $value)
-                        <div class="item @if($key==0) active @endif ">
-                            <img src="{{$value->path}}" alt="{{$value->name}}}}" width="460" height="345">
-                            <div class="carousel-caption">
-                                <h3>{{$value->name}}</h3>
-                            </div>
+                <div class="slider-content">
+                    @foreach($Param->pictures as $key => $photo)
+
+                        <div class="slider-single">
+                            <img class="slider-single-image" src="{{$photo->path}}" alt="{{$photo->name}}"/>
+                            <h1 class="slider-single-title">{{$photo->name}}</h1>
                         </div>
                     @endforeach
-
                 </div>
-
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
         </div>
     @endif
