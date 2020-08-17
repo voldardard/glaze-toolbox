36fcc6459356371189775660957ffd218edb5eb0
Brugger Mathias <brugger.mathias@hotmail.com>
2020-08-17 22:08:09 +0200

    display creator

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -54,5 +54,47 @@
         <div id="author">
             <span>Created by {{$Params->creator->fsname." ".$Params->creator->name}}</span>
         </div>
+        <div id="labels">
+            @foreach($Params->labels as $label)
+                <span class="label"><i class="fa fa-tag" aria-hidden="true"></i> {{$label}}</span>
+            @endforeach
+        </div>
     </div>
+    @if(!empty($Params->pictures))
+        <div id="carousel" class="container">
+            <br>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    @for($i=0; $i< count($Params->pictures); $i++)
+                        <li data-target="#myCarousel" data-slide-to="{{$i}}" @if($i==0)class="active"@endif></li>
+                    @endfor
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    @foreach($Params->pictures as $key => $value)
+                        <div class="item @if($key==0)active@endif">
+                            <img src="{{$value->path}}" alt="{{$value->name}}}}" width="460" height="345">
+                            <div class="carousel-caption">
+                                <h3>{{$value->name}}</h3>
+                            </div>
+                        </div>
+                    @endforeach
+
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    @endif
+
 </div>
