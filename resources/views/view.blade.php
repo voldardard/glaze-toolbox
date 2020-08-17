05128dab45c48d83061824bfcfd480ad1ef209ed
Brugger Mathias <brugger.mathias@hotmail.com>
2020-08-17 22:20:44 +0200

    display creator

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -22,12 +22,8 @@
 
 
     <!-- Scripts -->
-    <--
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    -->
-    <--
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+    <!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>-->
     <script src="{{ asset('js/carousel.js') }}"></script>
     <script type="text/javascript">
         var locale = "{{ str_replace('_', '-', app()->getLocale()) }}";
