a15a2f2875c601e21c6e3588c00e14332424d9b9
Brugger Mathias <brugger.mathias@hotmail.com>
2020-08-23 22:53:22 +0200

    escape to quit fullscreen

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Get the language requested
+$locale = \Request::segment(2);
+
+
+if (in_array($locale, array_keys(Config::get('app.availables_locale')))) {
+    App::setLocale($locale);
+}
+
+Route::group(['prefix' => Config::get('app.locale')], function () {
+    //authenticated route
+    Route::middleware("auth.classic")->group(function () {
+        Route::get('/category/{parentID?}', 'recipes\Categories@getCategory')->where(['parentID' => '[0-9]+']);
+        Route::get('/raw', 'recipes\Categories@getRaw');
+        Route::get('/lands', 'recipes\Categories@getLand');
+        Route::get('/sources/type', 'recipes\Categories@getType');
+        Route::get('/sources/author', 'recipes\Categories@getAuthor');
+        Route::get('/sources/author/type/{typeID?}', 'recipes\Categories@getAuthor')->where(['typeID' => '[0-9]+']);
+        Route::get('/jsonview/{recipeID}', 'recipes\Categories@buildView')->where(['recipeID' => '[a-zA-Z0-9]+']);
+
+    });
 });
