9b12488fd7187a2282a89fd98ad918dad8ef0a22
Brugger Mathias <mbrugger@net4all.ch>
2020-10-19 22:07:04 +0200

    s

--- resources/views/partials/categories.blade.php
+++ resources/views/partials/categories.blade.php
@@ -1,6 +1,6 @@
 @if(count($Params['childrens']) > 0)
   <ul>
-  @foreach ($Params->categories as $category)
+  @foreach ($Params['childrens'] as $category)
       <li>{{ $category['name'] }}</li>
       @include('partials.categories', ['Params'=>$category])
   @endforeach
