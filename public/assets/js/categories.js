2d8eea1e92573550d603e645909b717e37bebc62
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 11:57:32 +0200

    d

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -1,9 +1,15 @@
 function open_selector(id){
   element = document.getElementById('selector-list-'+id);
-  element.setAttribute("style", "display: block;")
+
+
+  element.addEventListener("mouseover", function (e) {
+      console.log('focus in of selector');
+        element.setAttribute("style", "display: block;")
+  });
 
   element.addEventListener("mouseout", function (e) {
       console.log('focus out of selector');
+      element.setAttribute("style", "display: none;")
   });
 
 }
