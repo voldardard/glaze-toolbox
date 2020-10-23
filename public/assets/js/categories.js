65df831841842572842325577ab0892800d49404
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 12:46:24 +0200

    c

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -1,6 +1,4 @@
 function selector(element, ul){
-  console.log('activate in');
-
   element.addEventListener("mouseover", function (e) {
       console.log('mouse hover of selector');
   });
@@ -9,8 +7,12 @@ function selector(element, ul){
         ul.setAttribute("style", "display: block;")
   });
 
-  element.addEventListener("mouseout", function (e) {
+/*  element.addEventListener("mouseout", function (e) {
       console.log('mouse out of selector');
+      ul.setAttribute("style", "display: none;")
+  });*/
+  element.addEventListener("focusout", function (e) {
+      console.log('focus out of selector');
       ul.setAttribute("style", "display: none;")
   });
 
