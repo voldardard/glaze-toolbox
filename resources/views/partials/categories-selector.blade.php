32fa8580715be7fbc3e04e710346fbd501729c68
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 11:10:21 +0200

    c

--- resources/views/partials/categories-selector.blade.php
+++ resources/views/partials/categories-selector.blade.php
@@ -1,7 +1,7 @@
 @if(count($Params->categories) > 0)
-<div id="{{ $category['id'] }}">
+<div class="selector" id="{{ $category['id'] }}">
   <span>Déplacer</span>
-  <ul class="selector_list">
+  <ul class="selector_list selector_disabled">
   @foreach ($Params->categories as $category)
     <li class="selector_item" onclick="change_category({{ $category['id'] }}, {{ $category['id'] }})">{{ $category['name'] }}</li>
     @include('partials.categories-selector-childrens', ['Params'=>$category])
