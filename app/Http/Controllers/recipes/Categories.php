6344242f5b05cc3c2c0ff07bed3e221ef8b1a48f
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 21:33:23 +0200

    d

--- app/Http/Controllers/recipes/Categories.php
+++ app/Http/Controllers/recipes/Categories.php
@@ -50,7 +50,7 @@ class Categories extends Controller{
               ]);
 
               print_r($categoryID);
-              print_r($equest->input()->all());
+              print_r($request->input()->all());
     }
 
     public function buildView($recipeID)
