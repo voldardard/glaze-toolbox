6559d6fcd2b9489c471938c3738c77acce0d70c1
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 22:30:34 +0200

    change insert.js

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -15,7 +15,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <!--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">-->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/insert.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/menu.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/autocomplete.css') }}">
@@ -23,8 +23,8 @@
 
 
     <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <!--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+  <!--  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>-->
     <script type="text/javascript">
         var locale = "{{ str_replace('_', '-', app()->getLocale()) }}";
         var lang = {
