cd7f3f5cbca328ab0fa96f92f54fec30687a4647
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 12:40:05 +0200

    d

--- resources/views/partials/categories-selector.blade.php
+++ resources/views/partials/categories-selector.blade.php
@@ -2,12 +2,13 @@
 <div class="selector" id="{{ $category['id'] }}">
   <span>Déplacer</span>
   <ul id="selector-list-{{ $category['id'] }}" class="selector_list selector_disabled">
-  @foreach ($Params->categories as $category)
-    <li class="selector_item" onclick="change_category({{ $category['id'] }}, {{ $category['id'] }})">{{ $category['name'] }}</li>
-    @include('partials.categories-selector-childrens', ['Params'=>$category])
+  @foreach ($Params->categories as $sub_category)
+    <li class="selector_item" onclick="change_category({{ $sub_category['id'] }}, {{ $sub_category['id'] }})">{{ $sub_category['name'] }}</li>
+    @include('partials.categories-selector-childrens', ['Params'=>$sub_category])
   @endforeach
   </ul>
   <script type="text/javascript">
+    console.log("Activate selector-list-{{ $category['id'] }}")
     selector(document.getElementById("selector-list-{{ $category['id'] }}"));
   </script>
 </div>
