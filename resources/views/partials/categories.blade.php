e867c010e27103bb873456138ff8af6e3cc73992
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 10:20:35 +0200

    d

--- resources/views/partials/categories.blade.php
+++ resources/views/partials/categories.blade.php
@@ -8,7 +8,7 @@
           <ul class="selector_list">
           @foreach ($Params['childrens'] as $category22)
             <li class="selector_item" onclick="change_category({{ $category['id'] }}, {{ $category22['id'] }})">{{ $category22['name'] }}</li>
-            @include('partials.categories', ['Params'=>$category])
+            @include('partials.categories', ['Params'=>$category22])
           @endforeach
           </ul>
         </div>
