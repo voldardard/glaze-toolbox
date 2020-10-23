f655f66a11238465a492ccf7a8ac35562a5b799e
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 20:53:34 +0200

    fetch

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -69,7 +69,7 @@ function update_name(id, value){
   var i= document.createElement('i');
   i.setAttribute("class", "fa fa-floppy-o");
   i.setAttribute("aria-hidden", "true");
-  i.setAttribute("id", 'category_name_dave-'+id);
+  i.setAttribute("id", 'category_name_save-'+id);
   i.setAttribute("onclick", "save_name("+(id)+")");
 
 
@@ -105,26 +105,67 @@ function cancel_name(id, value){
 
 }
 function save_name(id){
-  lmnt=document.getElementById('category_name_dave-'+id);
+  //loading
+  lmnt=document.getElementById('category_name_save-'+id);
     start_loading(lmnt, "fa-floppy-o");
+
+    //get new value
     value=document.getElementById('category_name_input-'+id).value;
     console.log(value);
-    const csrf = document.getElementsByName('_csrf-token')[0].content;
 
+    //get csrf and set headers
+    const csrf = document.getElementsByName('_csrf-token')[0].content;
+    const headers = new Headers({
+        'X-CSRF-TOKEN': csrf,
+        'accept': 'application/json'
+    });
+
+    //create object
     var category = {};
     category.id = id;
-    category._token = csrf;
     category.name = value;
 
-    $.ajax({
-        type: "PATCH",
-        url: "/category/"+id,
-        dataType: 'json',
-        data: category,
-        success: function(msg){
-            console.log(msg);
-            stop_loading(lmnt, 'fa-floppy-o');
+    fetch("/category/"+id, {
+        method: 'PATCH',
+        headers,
+        body: JSON.stringify(category),
+    }).then(function(response) {
+        console.log(response);
+        if (!response.ok) {
+            if(response.status===422){
+                response.json().then(data=>{
+                    for (let i in data['errors']){
+                        for (let j in data['errors'][i]) {
+                            alert_warning(translate('validationFailed') + data['errors'][i][j]);
+                            console.log(data['errors'][i][j][0]);
+
+                        }
+                    }
+                    stop_loading(iconId);
+                    document.getElementById(id).value="";
+                })
+            }else {
+                throw Error(response.statusText);
+            }
+        }else{
+            return response.json();
         }
+    }).then(data => {
+        // Work with JSON data here
+        /* console.log(data);*/
+        json_answer = data;
+        document.getElementById(id).value = "";
+        stop_loading(iconId);
+        //var json = JSON.parse(data);// here data is your response$
+        console.log(data);
+        for (var key in data) {
+            console.log(data[key]);
+            create_miniature(container, data[key]);
+        }
+    }).catch(function(error) {
+         /*   console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);*/
+        alert_warning(translate('problemConnecting') + error.message);
+        stop_loading(iconId);
     });
 
 }
