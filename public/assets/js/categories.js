840afdae46d67a1f404255691e87d08078b4f0bd
Brugger Mathias <mbrugger@net4all.ch>
2020-10-23 11:52:20 +0200

    f

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -2,7 +2,7 @@ function open_selector(id){
   element = document.getElementById('selector-list-'+id);
   element.setAttribute("style", "display: block;")
 
-  element.addEventListener("focusout", function (e) {
+  element.addEventListener("mouseout", function (e) {
       console.log('focus out of selector');
   });
 
