212107d5dd261200015f2bf45ca0ef3299190a5b
Brugger Mathias <mbrugger@net4all.ch>
2020-10-24 00:55:07 +0200

    hh

--- resources/views/partials/categories-selector-childrens.blade.php
+++ resources/views/partials/categories-selector-childrens.blade.php
@@ -1,6 +1,5 @@
 @if(count($Params['childrens']) > 0)
   <ul class="selector_list">
-    {{ $Params['name'] }}
   @foreach ($Params['childrens'] as $category)
     <li class="selector_item" onclick="change_category({{ $categoryID }}, {{ $category['id'] }})">{{ $category['name'] }}</li>
     @include('partials.categories-selector-childrens', ['Params'=>$category])
