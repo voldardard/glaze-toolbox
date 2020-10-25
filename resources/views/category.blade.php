5e2fb210425a01442442f4423b6b8410557653e6
Brugger Mathias <mbrugger@net4all.ch>
2020-10-25 23:05:27 +0100

    c

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -39,8 +39,12 @@
     <div class="category-page">
       @if(count($Params->recipes) > 0)
         <ul>
-          @foreach ($Params->recipes as recipe)
-            <li><span class="name">{{ $recipe['name'] }}</span><span class="version">{{ $recipe['version']}}</span> <span class="author">Created by {{ $recipe['users_fsname']." ".$recipe['users_name'] }}</span></li>
+          @foreach($Params->recipes as recipe)
+            <li>
+              <span class="name">{{ $recipe['name'] }}</span>
+              <span class="version">{{ $recipe['version'] }}</span>
+              <span class="author">Created by {{ $recipe['users_fsname']." ".$recipe['users_name'] }}</span>
+            </li>
           @endforeach
         </ul>
       @endif
