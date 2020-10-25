f9ada3e8e3766584e5c3eca951374520c02a29ca
Brugger Mathias <mbrugger@net4all.ch>
2020-10-25 12:23:45 +0100

    s

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -208,7 +208,7 @@ function choose_category(id, parent_id, name="NoName"){
 
 }
 function create_name(name, parent_id){
-  console.log(name);
+  console.log(name.trim());
   if ( name.trim()!='' && !isNaN(name) ){
     if ( parent_id.trim()!='' && !isNaN(parent_id) ){
       alert_success(name.trim()+parent_id.trim())
