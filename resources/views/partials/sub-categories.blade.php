dab8d184242e4c6421524d67f0d9dd8294ca3ad4
Brugger Mathias <mbrugger@net4all.ch>
2020-10-26 23:57:18 +0100

    create object for nested links

--- resources/views/partials/sub-categories.blade.php
+++ resources/views/partials/sub-categories.blade.php
@@ -1,6 +1,6 @@
 @foreach($Head['categories'] as $category)
-    <span class="block"><a href="/{{ str_replace('_', '-', app()->getLocale()) }}/category/{{$category['id']}}"><i
-                    class="fa fa-chevron-right"></i> {{$category['name']}}</a></span>
+    <span class="block"><object type="owo/uwu"><a href="/{{ str_replace('_', '-', app()->getLocale()) }}/category/{{$category['id']}}"><i
+                    class="fa fa-chevron-right"></i> {{$category['name']}}</a></object></span>
 @endforeach
 <i class="fa fa-chevron-right"></i>
 <h1 class="block">{{$Head['name']}}</h1><span class="block">{{$Head['version']}}</span>
