101f69796013dadbb47f9c4428c3857e24520daf
Brugger Mathias <mbrugger@net4all.ch>
2020-10-26 20:45:12 +0100

    c

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -5,7 +5,7 @@
       @foreach($Pictures as $picture)
       <div class="carousel-cell">
           <img  class="slider-single-image" src="{{$picture['path']}}" data-flickity-lazyload="{{$picture['path']}}" alt="{{$picture['name']}}"/>
-          <h1 stlye="display:none;" class="slider-single-title">{{$picture['name']}}</h1>
+          <h1 style="display:none;" class="slider-single-title">{{$picture['name']}}</h1>
       </div>
       @endforeach
     </div>
@@ -15,7 +15,7 @@
   @foreach($Pictures as $picture)
   <div class="carousel-cell">
       <img onclick="openInFullscreen(this)" class="slider-single-image" src="/pictures/no-pic.jpg" alt="Missing pic"/>
-      <h1 class="slider-single-title">Missing pic</h1>
+      <h1 style="display:none;" class="slider-single-title">Missing pic</h1>
   </div>
   @endforeach
 </div>
