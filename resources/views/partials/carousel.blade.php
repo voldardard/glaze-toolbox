f0052277f9047a6c655cb4e01b4edcdcd2d45f64
Brugger Mathias <mbrugger@net4all.ch>
2020-10-26 18:34:14 +0100

    add flickity

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,28 +1,24 @@
 @if(!empty($Pictures))
-    <div id="carousel">
-        <div class="slider-container">
 
-            <div class="slider-content">
-                @foreach($Pictures as $picture)
-                    <div class="slider-single">
-                        <img onclick="openInFullscreen(this)" class="slider-single-image" src="{{$picture['path']}}"
-                             alt="{{$picture['name']}}"/>
-                        <h1 class="slider-single-title">{{$picture['name']}}</h1>
-                    </div>
-                @endforeach
-            </div>
-        </div>
+    <!-- Flickity HTML init -->
+    <div class="gallery js-flickity" data-flickity-options='{ "wrapAround": true }'>
+      @foreach($Pictures as $picture)
+      <div class="gallery-cell">
+          <img onclick="openInFullscreen(this)" class="slider-single-image" src="{{$picture['path']}}"
+               alt="{{$picture['name']}}"/>
+          <h1 class="slider-single-title">{{$picture['name']}}</h1>
+      </div>
+      @endforeach
     </div>
 @else
-  <div id="carousel">
-      <div class="slider-container">
-          <div class="slider-content">
-              <div class="slider-single">
-                  <img onclick="openInFullscreen(this)" class="slider-single-image" src="/pictures/no-pic.jpg"
-                       alt="Missing picture"/>
-                  <h1 class="slider-single-title">Missing picture</h1>
-              </div>
-          </div>
-      </div>
+<!-- Flickity HTML init -->
+<div class="gallery js-flickity" data-flickity-options='{ "wrapAround": true }'>
+  @foreach($Pictures as $picture)
+  <div class="gallery-cell">
+      <img onclick="openInFullscreen(this)" class="slider-single-image" src="/pictures/no-pic.jpg"
+           alt="Missing pic"/>
+      <h1 class="slider-single-title">Missing pic</h1>
   </div>
+  @endforeach
+</div>
 @endif
