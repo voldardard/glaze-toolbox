9ffcc93239361fb720c29e1c302b25a5c0fe6de6
Brugger Mathias <mbrugger@net4all.ch>
2020-10-26 21:11:46 +0100

    c

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -5,7 +5,7 @@
       @foreach($Pictures as $picture)
       <div class="carousel-cell">
           <img  class="slider-single-image" data-flickity-lazyload="{{$picture['path']}}" alt="{{$picture['name']}}"/>
-          @if($disableTitle) <h1 style="position:absolute; bottom:0;" class="slider-single-title">{{$picture['name']}}</h1> @endif
+          <h1 style="position:absolute; bottom:0;" class="slider-single-title">{{$picture['name']}}</h1>
       </div>
       @endforeach
     </div>
