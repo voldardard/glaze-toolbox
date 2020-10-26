f465c25ec01d2f110774d421a0c0decb9a6cd483
Brugger Mathias <mbrugger@net4all.ch>
2020-10-26 22:35:21 +0100

    c

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -11,12 +11,10 @@
     </div>
 @else
 <!-- Flickity HTML init -->
-<div class="carousel js-flickity" data-flickity-options='{ "wrapAround": true }'>
-  @foreach($Pictures as $picture)
+<div class="carousel js-flickity" >
   <div class="carousel-cell">
-      <img onclick="openInFullscreen(this)" class="slider-single-image" src="/pictures/no-pic.jpg" alt="Missing pic"/>
+      <img class="slider-single-image" src="/pictures/no-pic.jpg" alt="Missing pic"/>
       <h1 style="display:none;" class="slider-single-title">Missing pic</h1>
   </div>
-  @endforeach
 </div>
 @endif
