b8d7f94d22a969acf46ba01077a063c4b7bb0198
Brugger Mathias <mbrugger@net4all.ch>
2020-10-26 19:00:10 +0100

    c

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,22 +1,20 @@
 @if(!empty($Pictures))
 
     <!-- Flickity HTML init -->
-    <div class="gallery js-flickity" data-flickity-options='{ "wrapAround": true }'>
+    <div class="carousel js-flickity" data-flickity-options='{ "wrapAround": true }'>
       @foreach($Pictures as $picture)
-      <div class="gallery-cell">
-          <img onclick="openInFullscreen(this)" class="slider-single-image" src="{{$picture['path']}}"
-               alt="{{$picture['name']}}"/>
+      <div class="carousel-cell">
+          <img  class="slider-single-image" src="{{$picture['path']}}" alt="{{$picture['name']}}"/>
           <h1 class="slider-single-title">{{$picture['name']}}</h1>
       </div>
       @endforeach
     </div>
 @else
 <!-- Flickity HTML init -->
-<div class="gallery js-flickity" data-flickity-options='{ "wrapAround": true }'>
+<div class="carousel js-flickity" data-flickity-options='{ "wrapAround": true }'>
   @foreach($Pictures as $picture)
-  <div class="gallery-cell">
-      <img onclick="openInFullscreen(this)" class="slider-single-image" src="/pictures/no-pic.jpg"
-           alt="Missing pic"/>
+  <div class="carousel-cell">
+      <img onclick="openInFullscreen(this)" class="slider-single-image" src="/pictures/no-pic.jpg" alt="Missing pic"/>
       <h1 class="slider-single-title">Missing pic</h1>
   </div>
   @endforeach
