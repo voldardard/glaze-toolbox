9939d88ad9312b8f24ecdff453775625dda79971
Brugger Mathias <mbrugger@net4all.ch>
2020-10-27 00:23:30 +0100

    c

--- resources/views/partials/recipes-list.blade.php
+++ resources/views/partials/recipes-list.blade.php
@@ -4,7 +4,7 @@
     @foreach($Params->recipes as $recipe)
     <li class="recipes-list_item">
       <div class="recipes-list_left-pic">
-        @include('partials.carousel',  ['Pictures'=>$recipe['pictures'], 'Carousel_options'=>'{ "wrapAround": true, "fullscreen": true, "lazyLoad": 1, "prevNextButtons": false, "pageDots": false }'])
+        @include('partials.carousel',  ['Pictures'=>$recipe['pictures'], 'Carousel_options'=>'{ "wrapAround": true, "fullscreen": true, "lazyLoad": true}'])
       </div>
       <a href="/{{ str_replace('_', '-', app()->getLocale()) }}/recipe/{{ $recipe['id'] }}" >
         <div class="recipes-list_right-text">
